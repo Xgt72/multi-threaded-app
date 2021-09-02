ba1909c795efccbaf93d7195dbcde22dbfbb959d
Xavier Georget <xageorget@gmail.com>
2021-09-02 17:01:22 +0200

    HeaderContainer

--- apps/covid/view/MainContainer.mjs
+++ apps/covid/view/MainContainer.mjs
@@ -1,46 +1,27 @@
-import Component    from '../../../node_modules/neo.mjs/src/component/Base.mjs';
-import TabContainer from '../../../node_modules/neo.mjs/src/tab/Container.mjs';
-import Viewport     from '../../../node_modules/neo.mjs/src/container/Viewport.mjs';
+import Viewport from "../../../node_modules/neo.mjs/src/container/Viewport.mjs";
+import HeaderContainer from "./HeaderContainer.mjs";
 
 /**
  * @class Covid.view.MainContainer
  * @extends Neo.container.Viewport
  */
 class MainContainer extends Viewport {
-    static getConfig() {return {
-        className: 'Covid.view.MainContainer',
-        autoMount: true,
-        layout   : {ntype: 'fit'},
-
-        items: [{
-            module: TabContainer,
-            height: 300,
-            width : 500,
-            style : {flex: 'none', margin: '20px'},
-
-            itemDefaults: {
-                module: Component,
-                cls   : ['neo-examples-tab-component'],
-                style : {padding: '20px'},
-            },
-
-            items: [{
-                tabButtonConfig: {
-                    iconCls: 'fa fa-home',
-                    text   : 'Tab 1'
-                },
-                vdom: {innerHTML: 'Welcome to your new Neo App.'}
-            }, {
-                tabButtonConfig: {
-                    iconCls: 'fa fa-play-circle',
-                    text   : 'Tab 2'
-                },
-                vdom: {innerHTML: 'Have fun creating something awesome!'}
-            }]
-        }]
-    }}
+  static getConfig() {
+    return {
+      className: "Covid.view.MainContainer",
+      layout: { ntype: "vbox", align: "stretch" },
+      itemDefaults: {
+        ntype: "component",
+      },
+      items: [
+        HeaderContainer,
+        { vdom: { innerHTML: "Center" } },
+        { vdom: { innerHTML: "Footer" } },
+      ],
+    };
+  }
 }
 
 Neo.applyClassConfig(MainContainer);
 
-export {MainContainer as default};
+export { MainContainer as default };
